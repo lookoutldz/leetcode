4dc3f9559cee1fa981d098c1f6fd45adfb1626d0
looko <lookoutldz@outlook.com>
2020-05-08 23:52:10 +0800

    day9 fix bug and another way.

--- src/day9/CombineList.java
+++ src/day9/CombineList.java
@@ -7,40 +7,66 @@ import java.util.List;
 public class CombineList {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = l1.val > l2.val ? l2 : l1, cur1 = l1, cur2 = cur1, temp;
-        while (l2 != null && cur1.next != null) {
-            if (l2.val < cur1.val) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        ListNode head, temp, key;
+        if (l2.val < l1.val) {
+            head = l2;
+            temp = l2.next;
+            l2.next = l1;
+            l2 = temp;
+            l1 = head;
+        } else {
+            head = l1;
+        }
+        key = head;
+        while (l2 != null && l1 != null) {
+            if (l2.val < l1.val) {
                 temp = l2.next;
-                l2.next = cur1;
-                cur1 = l2;
+                l2.next = l1;
+                key.next = l2;
+                key = l2;
                 l2 = temp;
             } else {
-                cur2 = cur1;
-                cur1 = cur1.next;
+                key = l1;
+                l1 = l1.next;
+                if (l1 == null) {
+                    key.next = l2;
+                }
             }
         }
         return head;
     }
 
-    public void test() {
-        ListNode l1 = new ListNode(1), key;
-        key = l1;
-        for (int i = 2; i < 7; i+=2) {
-            ListNode n = new ListNode(i);
-            key.next = n;
-            key = key.next;
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode preHead = new ListNode(0), temp = preHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
         }
-        PrintUtils.print(l1);
-        ListNode l2 = new ListNode(1);
-        key = l2;
-        for (int i = 3; i < 7; i+=2) {
-            ListNode n = new ListNode(i);
-            key.next = n;
-            key = key.next;
-        }
-        PrintUtils.print(l2);
+        temp.next = l1 == null ? l2 : l1;
+        return preHead.next;
+    }
 
-        ListNode result = mergeTwoLists(l1, l2);
+    public void test() {
+        ListNode a = new ListNode(5);
+        PrintUtils.print(a);
+
+        ListNode b = new ListNode(1);
+        ListNode b1 = new ListNode(2);
+        b.next = b1;
+        ListNode b2 = new ListNode(4);
+        b1.next = b2;
+        PrintUtils.print(b);
+
+        ListNode result = mergeTwoLists2(a, b);
         PrintUtils.print(result);
     }
 }
