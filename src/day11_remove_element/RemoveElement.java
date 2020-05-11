b88b253ca8af9b1b280829b4673408a00d480fc3
looko <lookoutldz@outlook.com>
2020-05-11 08:08:32 +0800

    day11 remove element, update gitignore

---
+++ src/day11_remove_element/RemoveElement.java
@@ -0,0 +1,17 @@
+package day11_remove_element;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length < 1) {
+            return 0;
+        }
+        int len = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[len] = nums[i];
+                len++;
+            }
+        }
+        return len;
+    }
+}
