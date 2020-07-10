c36b8e03c5e75e21c35d29adb0cfcb8e3b768061
looko <lookoutldz@outlook.com>
2020-07-10 09:02:24 +0800

    improve day31 recursive way

--- src/day31/FindRouteInTree.java
+++ src/day31/FindRouteInTree.java
@@ -8,6 +8,16 @@ public class FindRouteInTree {
         return root != null && recursive(root, 0, sum);
     }
 
+    private boolean hasPathSum2(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum - root.val == 0;
+        }
+        return hasPathSum2(root.left, sum - root.val) || hasPathSum2(root.right, sum - root.val);
+    }
+
     private boolean recursive(TreeNode node, int ancestorSum, int sum) {
         if (node.left != null && recursive(node.left, ancestorSum + node.val, sum)) {
             return true;
@@ -15,8 +25,8 @@ public class FindRouteInTree {
         if (node.right != null && recursive(node.right, ancestorSum + node.val, sum)) {
             return true;
         }
-        if (node.left == null && node.right == null && ancestorSum + node.val == sum) {
-            return true;
+        if (node.left == null && node.right == null) {
+            return ancestorSum + node.val == sum;
         }
         return false;
     }
